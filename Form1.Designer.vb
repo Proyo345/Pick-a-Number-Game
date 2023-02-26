edffb13d7e31a519d87ad754734a7d43579de10a
2020elsmith <2020elismith@gmail.com>
2023-02-26 16:24:59 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.txtGuess = New System.Windows.Forms.TextBox()
+        Me.btnNo = New System.Windows.Forms.Button()
+        Me.btnYes = New System.Windows.Forms.Button()
+        Me.lblVictory = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(293, 237)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(199, 91)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'txtGuess
+        '
+        Me.txtGuess.Location = New System.Drawing.Point(296, 173)
+        Me.txtGuess.Name = "txtGuess"
+        Me.txtGuess.Size = New System.Drawing.Size(196, 23)
+        Me.txtGuess.TabIndex = 2
+        '
+        'btnNo
+        '
+        Me.btnNo.Location = New System.Drawing.Point(223, 244)
+        Me.btnNo.Name = "btnNo"
+        Me.btnNo.Size = New System.Drawing.Size(145, 78)
+        Me.btnNo.TabIndex = 3
+        Me.btnNo.Text = "No"
+        Me.btnNo.UseVisualStyleBackColor = True
+        '
+        'btnYes
+        '
+        Me.btnYes.Location = New System.Drawing.Point(412, 242)
+        Me.btnYes.Name = "btnYes"
+        Me.btnYes.Size = New System.Drawing.Size(164, 80)
+        Me.btnYes.TabIndex = 4
+        Me.btnYes.Text = "Yes"
+        Me.btnYes.UseVisualStyleBackColor = True
+        '
+        'lblVictory
+        '
+        Me.lblVictory.AutoSize = True
+        Me.lblVictory.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblVictory.Location = New System.Drawing.Point(223, 187)
+        Me.lblVictory.Name = "lblVictory"
+        Me.lblVictory.Size = New System.Drawing.Size(330, 32)
+        Me.lblVictory.TabIndex = 5
+        Me.lblVictory.Text = "Would you like to play again?"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(809, 481)
+        Me.Controls.Add(Me.lblVictory)
+        Me.Controls.Add(Me.btnYes)
+        Me.Controls.Add(Me.btnNo)
+        Me.Controls.Add(Me.txtGuess)
+        Me.Controls.Add(Me.btnStart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnStart As Button
+    Friend WithEvents txtGuess As TextBox
+    Friend WithEvents btnNo As Button
+    Friend WithEvents btnYes As Button
+    Friend WithEvents lblVictory As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        Getnumber()
+        btnNo.Visible = False
+        btnYes.Visible = False
+        lblVictory.Visible = False
+    End Sub
+    Dim guess As Integer
+    Private Sub Getnumber()
+
+        guess = Math.Ceiling(Rnd() * 100)
+
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        Select Case (Val(txtGuess.Text))
+            Case Is > guess
+                MsgBox("Too High")
+            Case Is < guess
+                MsgBox("Too Low")
+            Case Is = guess
+                MsgBox("Thats correct!")
+                txtGuess.Visible = False
+                btnStart.Visible = False
+                btnNo.Visible = True
+                btnYes.Visible = True
+                lblVictory.Visible = True
+        End Select
+
+        txtGuess.Text = ""
+
+    End Sub
+
+    Private Sub btnNo_Click(sender As Object, e As EventArgs) Handles btnNo.Click
+        End
+    End Sub
+
+    Private Sub btnYes_Click(sender As Object, e As EventArgs) Handles btnYes.Click
+        Getnumber()
+        txtGuess.Visible = True
+        btnStart.Visible = True
+        btnNo.Visible = False
+        btnYes.Visible = False
+        lblVictory.Visible = False
+    End Sub
+End Class
